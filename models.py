61c763a05095792cd907c61adb6e2ef7b4fca0ac
Thien Kian Foh <thienkianfoh@gmail.com>
2024-09-18 02:17:25 +0800

    Removed testing attribute and modify attribute constraints

--- models.py
+++ models.py
@@ -8,8 +8,8 @@ from geoalchemy2 import Geography  # For spatial data
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)  # PK
-    username = db.Column(db.String(100), unique=True, nullable=False)  # Name from Google signIn
-    email = db.Column(db.String(120), unique=True, nullable=False)  # email from Google signIn
+    username = db.Column(String, unique=True, nullable=False)  # Name from Google signIn
+    email = db.Column(String, unique=True, nullable=False)  # email from Google signIn
     isadmin = db.Column(db.Boolean, default=False)  # Admin authorization 
 
     def __repr__(self): 
@@ -18,7 +18,7 @@ class Users(db.Model):
 class Categories(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)  # PK
-    name = db.Column(db.String(80), nullable=False, unique=True)  # Category name
+    name = db.Column(String, nullable=False, unique=True)  # Category name
 
     def __repr__(self):
         return f'<Category {self.name}>'
@@ -43,14 +43,12 @@ class Locations(db.Model):
 
 class Merchants(db.Model):
     __tablename__ = 'merchants'
-    
     id = db.Column(db.Integer, primary_key=True)  # PK
-    name = db.Column(db.String(120), nullable=False)  # Merchant name
+    name = db.Column(String, nullable=False)  # Merchant name
     category_id = db.Column(db.Integer, ForeignKey('categories.id'), nullable=False)  # FK to Categories
     discount = db.Column(Text, nullable=False)  # Multiline string for discount
     more_info = db.Column(Text, nullable=True)  # Multiline string for additional info
     terms = db.Column(Text, nullable=False)  # Multiline string for terms
-    testing = db.Column(Text, nullable=False)  # Multiline string for terms
     image = db.Column(String, nullable=True)  # URL to the image
 
     category = relationship('Categories', backref='merchants')
