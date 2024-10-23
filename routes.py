3fd81a9d49bc301148d0235612aa8ad00e367bd7
Thien Kian Foh <thienkianfoh@gmail.com>
2024-10-23 16:28:11 +0800

    - Change add request_merchant endpoint to more appropriate

--- routes.py
+++ routes.py
@@ -149,7 +149,7 @@ def remove_admin(verified_email):
     return jsonify({'message': 'Admin email removed successfully'}), 200
 
 # Add admin email
-@main.route('/add_request_merchant', methods=['POST'])
+@main.route('/request_merchant', methods=['POST'])
 @google_token_required
 def add_request_merchant(verified_email):
     # Log the API call with the verified email
