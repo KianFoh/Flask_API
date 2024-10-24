e22d0750ca8e4466dd7196a96b25d9221895d576
Thien Kian Foh <thienkianfoh@gmail.com>
2024-10-24 20:53:22 +0800

    - Update requirements.txt and use waitress with eventlet to host for production

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+# Apply eventlet monkey patching first
+import eventlet
+eventlet.monkey_patch()
+
 from app_config import create_app
 from socketio_events import socketio
 import logging
 
+# Create the app
 app = create_app()
 
-if __name__ == '__main__':
-    # Set the logging level for Werkzeug to WARNING to suppress detailed HTTP request logs
-    # log = logging.getLogger('werkzeug')
-    # log.setLevel(logging.WARNING)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+# Ensure that the app context is used where needed
+with app.app_context():
+    if __name__ == '__main__':
+        # Set the logging level for Werkzeug to WARNING to suppress detailed HTTP request logs
+        # log = logging.getLogger('werkzeug')
+        # log.setLevel(logging.WARNING)
+        socketio.run(app, host='0.0.0.0', port=5000, debug=False)
